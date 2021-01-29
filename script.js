29dc572fb7d8f88ad934eaf4225dd0d198e5ec9b
Aakashshinde <61353085+Aakashshinde@users.noreply.github.com>
2021-01-30 00:13:54 +0530

    Update script.js
    Added colons

--- script.js
+++ script.js
@@ -1,43 +1,43 @@
-let ultasks = $('#ultasks')
-let btnadd  = $('#btnadd')
-let btnclear= $('#btnclear')
-let inpnewtask = $('#inpnewtask')
-let btnsort = $('#btnsort')
-let btncleanup = $('#btncleanup')
+let ultasks = $('#ultasks');
+let btnadd  = $('#btnadd');
+let btnclear= $('#btnclear');
+let inpnewtask = $('#inpnewtask');
+let btnsort = $('#btnsort');
+let btncleanup = $('#btncleanup');
 
 function cleardone() {
-    $('#ultasks .done').remove()
+    $('#ultasks .done').remove();
 }
 
 function sortitems(){
-    let items = $('ultasks .done') // dashed items will be stored here
-    cleardone()                   //   it will clear all dashed items
-    ultasks.append(items)         //  now dashed items will be added to bottom
+    let items = $('ultasks .done'); // dashed items will be stored here
+    cleardone();                 //   it will clear all dashed items
+    ultasks.append(items);         //  now dashed items will be added to bottom
 }
 function addItem(){
     let listitem = $('<li>',{
         'class' : 'list-group-item',
-         text: inpnewtask.val()
+         text: inpnewtask.val();
     })
     ultasks.append(listitem)
 
     listitem.click((event)=> {
-        $(event.target).toggleClass('done')
+        $(event.target).toggleClass('done');
     })
 }
 inpnewtask.keypress((e) =>{
-    if(e.which == 13) addItem()
+    if(e.which == 13) addItem();
 })
 
 
 btnadd.click(() => addItem())
-btnsort.click(sortitems)
+btnsort.click(sortitems);
 
 
 btnclear.click(() => {
-    inpnewtask.val('')
+    inpnewtask.val('');
 })
 
 
-btncleanup.click(cleardone)
+btncleanup.click(cleardone);
 
